11afae61b33c814e4871b6ce736e3aa44da190ee
Ariel Ramirez <arielramirez20395@gmail.com>
2020-05-31 00:11:05 -0300

    Move navbar to component Header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,6 @@
 <template>
   <div id="app">
-    <b-navbar id="nav" toggleable="lg" type="dark" variant="dark">
-      <div class="container">
-        <b-navbar-brand href="#">
-          <router-link to="/">Home</router-link>
-        </b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">
-              <router-link to="/cart">Cart <font-awesome-icon icon="shopping-cart"/></router-link>
-            </b-nav-item>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>        
-      </div>
-    </b-navbar>
+    <Header/>
     <router-view class="container"/>
   </div>
 </template>
@@ -33,18 +12,15 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
-#nav {
-  font-weight: bold;
-  background-color: #082f61 !important;
-}
-
-#nav a {
-  color: #fff;
-  text-decoration: none;
-}
-
-.nav-collapse {
-  text-align: right
-}
 </style>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/header/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <b-navbar id="nav" toggleable="lg" type="dark" variant="dark">
+      <div class="container">
+        <b-navbar-brand href="#">
+          <router-link to="/">Home</router-link>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">
+              <router-link to="/cart">Cart <font-awesome-icon icon="shopping-cart"/></router-link>
+            </b-nav-item>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item  href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>        
+      </div>
+    </b-navbar>
+</template>
+
+<style scoped>
+#nav {
+  font-weight: bold;
+  background-color: #082f61 !important;
+}
+
+#nav a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.nav-collapse {
+  text-align: right
+}
+
+ul.dropdown-menu a {
+    color: #082f61 !important;
+}
+</style>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
