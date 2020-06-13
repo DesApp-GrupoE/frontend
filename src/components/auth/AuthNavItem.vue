95d020a1b4e69a555e9b9d69fc0f25abcb4da0be
Ariel Ramirez <arielramirez20395@gmail.com>
2020-06-13 14:22:31 -0300

    feat(auth): add AuthNavItem to show modal of login

---
+++ src/components/auth/AuthNavItem.vue
@@ -0,0 +1,50 @@
+<template>
+	<b-nav-item right>
+		<!--  Login  -->
+		<div class="d-flex justify-content-end align-items-center auth" v-on:click="showModal">
+			<font-awesome-icon icon="user-circle"/> <span>Sign In</span>
+		</div>
+
+		<b-modal v-model="modalShow" title="Login" hide-footer>
+			<div class="row m-2 justify-content-end">
+				<input type="text" class="form-control input-login" placeholder="email">
+				<input type="password" class="form-control input-login" placeholder="password">
+			</div>
+			<button class="btn btn-primary float-right">Login</button>
+		</b-modal>
+	</b-nav-item>
+</template>
+
+<style scoped>
+.nav-link {
+	padding: 0;
+}
+
+.input-login {
+	margin-bottom: 10px;
+}
+
+.auth {
+	color: #fff;
+}
+
+.fa-user-circle {
+	margin-right: 5px;
+}
+</style>
+
+<script>
+export default {
+	name: 'AuthNavItem',
+	data() {
+		return {
+			modalShow: false
+		}
+	},
+	methods: {
+		showModal: function() {
+			this.modalShow = true;
+		}
+	}
+}
+</script>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,34 +1,43 @@
 <template>
-    <b-navbar id="nav" toggleable="lg" type="dark" variant="dark">
-      <div class="container">
-        <b-navbar-brand href="#">
+  <div>
+    <b-navbar id="nav" toggleable="sm" type="dark" variant="dark">
+      <div class="container d-flex align-items-start">
+        <b-navbar-brand>
           <router-link to="/">Home</router-link>
         </b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <div class="d-flex-justify-content-end">
+          <AuthNavItem class="d-lg-block d-none" />
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">
-              <router-link to="/cart">{{$t("Cart")}} <font-awesome-icon icon="shopping-cart"/></router-link>
-            </b-nav-item>
+          <div class="d-flex justify-content-end align-items-center">
+            <AuthNavItem class="d-flex justify-content-center align-items-center d-lg-none auth-dropdown" />
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          </div>
 
-            <b-nav-item-dropdown right>
-              <!--  Language  -->
-              <template slot="button-content">
-                <span>{{$t('Language')}}</span> <font-awesome-icon icon="globe-americas"/>
-              </template>
-              <b-dropdown-item href="#" v-for="(lang, i) in langs" :key="`Lang${i}`" 
-                                :value="lang" @click="changeLanguage(lang)">
-                <div class="d-flex justify-content-between">
-                  {{$t('lang.'+ lang)}}
-                </div>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>        
-          </b-navbar-nav>
-        </b-collapse>        
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto pull-right">
+              <b-nav-item-dropdown right>
+                <!--  Language  -->
+                <template slot="button-content">
+                  <font-awesome-icon icon="globe-americas"/> <span>{{$t('Language')}}</span>
+                </template>
+                <b-dropdown-item href="#" v-for="(lang, i) in langs" :key="`Lang${i}`"
+                                  :value="lang" @click="changeLanguage(lang)">
+                  <div class="d-flex justify-content-between">
+                    {{$t('lang.'+ lang)}}
+                  </div>
+                </b-dropdown-item>
+              </b-nav-item-dropdown>
+
+              <b-nav-item>
+                <router-link to="/cart"><font-awesome-icon icon="shopping-cart"/> {{$t("Cart")}}</router-link>
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
       </div>
     </b-navbar>
+  </div>
 </template>
 
 <style scoped>
@@ -54,11 +63,20 @@ ul.dropdown-menu a {
   width: 20px;
   height: 20px;
 }
+
+.auth-dropdown {
+  margin-right: 10px;
+}
 </style>
 
 <script>
+import AuthNavItem from '@/components/auth/AuthNavItem.vue';
+
 export default {
     name: 'Header',
+    components: {
+      AuthNavItem
+    },
     data () {
       return { 
         langs: ['es', 'en'] 
